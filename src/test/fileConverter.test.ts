82e3b8e89d16309e7f21a9135c233d540fd97baa
rscrivens <34690530+rscrivens@users.noreply.github.com>
2019-06-03 14:32:53 -0700

    update. add and clean up the File Converter Tests

--- src/test/fileConverter.test.ts
+++ src/test/fileConverter.test.ts
@@ -9,78 +9,116 @@ import { FileConverter } from "../FileConverter";
 
 suite("canUpgradeVersion", () => {
     // @ts-ignore parseVersion is private
-    const v2FullVersion = FileConverter.parseVersion("2.0.0-csd.2.beta.2018-10-10");
+    const v2FullCSDVersion = FileConverter.parseVersion("2.0.0-csd.2.beta.2018-10-10");
+    // @ts-ignore parseVersion is private
+    const v2FullRTMVersion = FileConverter.parseVersion("2.0.0-rtm.2");
     // @ts-ignore parseVersion is private
     const v2Version = FileConverter.parseVersion("2.0.0");
 
+    const older = true;
+    const newer = false;
+    const same = false;
+
     /**
-     * sets the current version, calls the canUpgradeVersion function and verifies the expected result
+     * sets the current version, calls the isOlderThenVersion function and verifies the expected result
      * @param curVersion current SarifVersion to set as curVersion
      * @param compareVersion version to check against current
-     * @param expectedEqual expected return value of the canUpgradeVersion call
+     * @param expectedEqualValue expected return value of the isOlderThenVersion call
      */
-    function callCanUpgradeVersion(curVersion: SarifVersion, compareVersion: string, expectedEqual: boolean) {
+    function callIsOlderThenVersion(compareVersion: string, curVersion: SarifVersion, expectedEqualValue: boolean) {
         // @ts-ignore curVersion is private
         FileConverter.curVersion = curVersion;
 
-        // @ts-ignore canUpgradeVersion is private but ignoring so we can call directly
-        const canUpgrade = FileConverter.canUpgradeVersion(compareVersion);
+        // @ts-ignore parseVersion is private
+        const parsedVer = FileConverter.parseVersion(compareVersion);
 
-        assert.equal(canUpgrade, expectedEqual);
+        // @ts-ignore isOlderThenVersion is private but ignoring so we can call directly
+        const canUpgrade = FileConverter.isOlderThenVersion(parsedVer, curVersion);
+
+        assert.equal(canUpgrade, expectedEqualValue, `Parsed: ${parsedVer.original}, Current: ${curVersion.original}`);
     }
 
     test("v1", () => {
-        const version = "1.0.0";
-        callCanUpgradeVersion(v2FullVersion, version, true);
-        callCanUpgradeVersion(v2Version, version, true);
+        const fileVersion = "1.0.0";
+        callIsOlderThenVersion(fileVersion, v2FullRTMVersion, older);
+        callIsOlderThenVersion(fileVersion, v2Version, older);
+        callIsOlderThenVersion(fileVersion, v2FullCSDVersion, older);
     });
 
     test("v1 with csd", () => {
-        const version = "1.0.0-csd.2.beta.2018-10-10";
-        callCanUpgradeVersion(v2FullVersion, version, true);
-        callCanUpgradeVersion(v2Version, version, true);
+        const fileVersion = "1.0.0-csd.2.beta.2018-10-10";
+        callIsOlderThenVersion(fileVersion, v2FullRTMVersion, older);
+        callIsOlderThenVersion(fileVersion, v2Version, older);
+        callIsOlderThenVersion(fileVersion, v2FullCSDVersion, older);
     });
 
-    test("v2", () => {
-        const version = "2.0.0";
-        callCanUpgradeVersion(v2FullVersion, version, true);
-        callCanUpgradeVersion(v2Version, version, false);
+    test("matches v2Version", () => {
+        const fileVersion = "2.0.0";
+        callIsOlderThenVersion(fileVersion, v2FullRTMVersion, older);
+        callIsOlderThenVersion(fileVersion, v2Version, same);
+        callIsOlderThenVersion(fileVersion, v2FullCSDVersion, newer);
     });
 
     test("v2 older csd", () => {
-        const version = "2.0.0-csd.1.beta.2018-5-10";
-        callCanUpgradeVersion(v2FullVersion, version, true);
-        callCanUpgradeVersion(v2Version, version, false);
+        const fileVersion = "2.0.0-csd.1.beta.2018-5-10";
+        callIsOlderThenVersion(fileVersion, v2FullRTMVersion, older);
+        callIsOlderThenVersion(fileVersion, v2Version, older);
+        callIsOlderThenVersion(fileVersion, v2FullCSDVersion, older);
     });
 
     test("v2 older csd date", () => {
-        const version = "2.0.0-csd.2.beta.2018-8-10";
-        callCanUpgradeVersion(v2FullVersion, version, true);
-        callCanUpgradeVersion(v2Version, version, false);
+        const fileVersion = "2.0.0-csd.2.beta.2018-8-10";
+        callIsOlderThenVersion(fileVersion, v2FullRTMVersion, older);
+        callIsOlderThenVersion(fileVersion, v2Version, older);
+        callIsOlderThenVersion(fileVersion, v2FullCSDVersion, older);
     });
 
-    test("current version", () => {
-        const version = "2.0.0-csd.2.beta.2018-10-10";
-        callCanUpgradeVersion(v2FullVersion, version, false);
-        callCanUpgradeVersion(v2Version, version, false);
+    test("matches v2FullCSDVersion", () => {
+        const fileVersion = "2.0.0-csd.2.beta.2018-10-10";
+        callIsOlderThenVersion(fileVersion, v2FullRTMVersion, older);
+        callIsOlderThenVersion(fileVersion, v2Version, older);
+        callIsOlderThenVersion(fileVersion, v2FullCSDVersion, same);
     });
 
     test("v2 newer csd date", () => {
-        const version = "2.0.0-csd.2.beta.2018-12-10";
-        callCanUpgradeVersion(v2FullVersion, version, false);
-        callCanUpgradeVersion(v2Version, version, false);
+        const fileVersion = "2.0.0-csd.2.beta.2018-12-10";
+        callIsOlderThenVersion(fileVersion, v2FullRTMVersion, older);
+        callIsOlderThenVersion(fileVersion, v2Version, older);
+        callIsOlderThenVersion(fileVersion, v2FullCSDVersion, newer);
+    });
+
+    test("v2 rtm older", () => {
+        const fileVersion = "2.0.0-rtm.1";
+        callIsOlderThenVersion(fileVersion, v2FullRTMVersion, older);
+        callIsOlderThenVersion(fileVersion, v2Version, newer);
+        callIsOlderThenVersion(fileVersion, v2FullCSDVersion, newer);
+    });
+
+    test("matches v2FullRTMVersion", () => {
+        const fileVersion = "2.0.0-rtm.2";
+        callIsOlderThenVersion(fileVersion, v2FullRTMVersion, same);
+        callIsOlderThenVersion(fileVersion, v2Version, newer);
+        callIsOlderThenVersion(fileVersion, v2FullCSDVersion, newer);
+    });
+
+    test("v2 rtm newer", () => {
+        const fileVersion = "2.0.0-rtm.3";
+        callIsOlderThenVersion(fileVersion, v2FullRTMVersion, newer);
+        callIsOlderThenVersion(fileVersion, v2Version, newer);
+        callIsOlderThenVersion(fileVersion, v2FullCSDVersion, newer);
     });
 
     test("v3", () => {
-        const version = "3.0.0";
-        callCanUpgradeVersion(v2FullVersion, version, false);
-        callCanUpgradeVersion(v2Version, version, false);
+        const fileVersion = "3.0.0";
+        callIsOlderThenVersion(fileVersion, v2FullRTMVersion, newer);
+        callIsOlderThenVersion(fileVersion, v2Version, newer);
+        callIsOlderThenVersion(fileVersion, v2FullCSDVersion, newer);
     });
 });
 
 suite("parseVersion", () => {
     /**
-     * Seperated this call to reduce the number of @ts-ignore comments as parseVerion is private
+     * Seperated this call to reduce the number of @ts-ignore comments as parseVersion is private
      * @param version version to parse
      */
     function callParseVersion(version: string): SarifVersion {
@@ -88,24 +126,67 @@ suite("parseVersion", () => {
         return FileConverter.parseVersion(version);
     }
 
-    test("full version", () => {
-        const parsedVer = callParseVersion("2.0.0-csd.2.beta.2018-9-10");
+    test("full csd version", () => {
+        const version = "2.0.0-csd.2.beta.2018-9-10";
+        const parsedVer = callParseVersion(version);
         assert.deepEqual(parsedVer, {
-            csd: 2, csdDate: new Date(2018, 9, 10), major: 2, minor: 0, sub: 0,
+            csd: 2, csdDate: new Date(2018, 9, 10), major: 2, minor: 0, original: version, sub: 0,
         } as SarifVersion);
+    });
+
+    test("rtm version", () => {
+        const version = "2.0.0-rtm.2";
+        const parsedVer = callParseVersion(version);
+        assert.deepEqual(parsedVer, { major: 2, minor: 0, original: version, rtm: 2, sub: 0 } as SarifVersion);
     });
 
     test("v1", () => {
-        const parsedVer = callParseVersion("1.0.0");
-        assert.deepEqual(parsedVer, {
-            major: 1, minor: 0, sub: 0,
-        } as SarifVersion);
+        const version = "1.0.0";
+        const parsedVer = callParseVersion(version);
+        assert.deepEqual(parsedVer, { major: 1, minor: 0, original: version, sub: 0 } as SarifVersion);
     });
 
     test("v1.2.3", () => {
-        const parsedVer = callParseVersion("1.2.3");
+        const version = "1.2.3";
+        const parsedVer = callParseVersion(version);
+        assert.deepEqual(parsedVer, { major: 1, minor: 2, original: version, sub: 3 } as SarifVersion);
+    });
+});
+
+suite("parseSchemaVersion", () => {
+    /**
+     * Seperated this call to reduce the number of @ts-ignore comments as parseSchema is private
+     * @param version version to parse
+     */
+    function callParseSchema(version: string): SarifVersion {
+        const schemaPrefix = "http://json.schemastore.org/sarif-";
+        // @ts-ignore parseSchema is private but ingnoring for testing
+        return FileConverter.parseSchema(schemaPrefix + version);
+    }
+
+    test("full csd version", () => {
+        const version = "2.0.0-csd.2.beta.2018-9-10";
+        const parsedVer = callParseSchema(version);
         assert.deepEqual(parsedVer, {
-            major: 1, minor: 2, sub: 3,
+            csd: 2, csdDate: new Date(2018, 9, 10), major: 2, minor: 0, original: version, sub: 0,
         } as SarifVersion);
+    });
+
+    test("rtm version", () => {
+        const version = "2.0.0-rtm.2";
+        const parsedVer = callParseSchema(version);
+        assert.deepEqual(parsedVer, { major: 2, minor: 0, original: version, rtm: 2, sub: 0 } as SarifVersion);
+    });
+
+    test("v1", () => {
+        const version = "1.0.0";
+        const parsedVer = callParseSchema(version);
+        assert.deepEqual(parsedVer, { major: 1, minor: 0, original: version, sub: 0 } as SarifVersion);
+    });
+
+    test("v1.2.3", () => {
+        const version = "1.2.3";
+        const parsedVer = callParseSchema(version);
+        assert.deepEqual(parsedVer, { major: 1, minor: 2, original: version, sub: 3 } as SarifVersion);
     });
 });
