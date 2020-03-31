cb932037bc323fc4202371c9d84ea1671a2923b8
Gabriel S DeBacker <g_debacker@hotmail.com>
2020-03-30 17:15:08 -0700

    Add missing file

--- src/ExplorerController.ts
+++ src/ExplorerController.ts
@@ -289,11 +289,7 @@ export class ExplorerController implements Disposable {
             selectedRow: this.selectedCodeFlowRow,
             selectedVerbosity: this.selectedVerbosity,
             resultInfo: this.activeDiagnostic.resultInfo,
-<<<<<<< HEAD
             runInfo: this.activeDiagnostic.resultInfo.runInfo
-=======
-            runInfo: this.activeDiagnostic.runInfo
->>>>>>> origin/master
         };
 
         const dataString: string = JSON.stringify(diagData);
