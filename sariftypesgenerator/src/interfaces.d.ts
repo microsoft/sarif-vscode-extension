4e7217547bd70a736994caf07d48b95a452af165
Jeff King <jeffking@gmail.com>
2020-05-26 12:46:09 -0700

    Remove sariftypesgenerator.

--- sariftypesgenerator/src/interfaces.d.ts
+++
@@ -1,9 +0,0 @@
-// /********************************************************
-// *                                                       *
-// *   Copyright (C) Microsoft. All rights reserved.       *
-// *                                                       *
-// ********************************************************/
-export interface SarifEnum {
-    name: string;
-    values: string[];
-}
