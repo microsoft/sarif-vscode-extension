f1f478ad808c02090155aa651e106717db153b3b
Jeff King <jeffking@gmail.com>
2020-06-03 19:18:27 -0700

    Always npm install before npm start (F5).

--- install.js
+++
@@ -1,8 +0,0 @@
-// F5 -> npm start -> npm prestart -> this script.
-// Runs `npm install` only if needed. Otherwise skips (and saves time).
-
-const fs = require('fs')
-const { execSync } = require('child_process')
-
-if (!fs.existsSync('node_modules'))
-	execSync('npm install', { stdio: 'inherit' })
