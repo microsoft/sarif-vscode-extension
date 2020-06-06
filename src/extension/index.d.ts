c9c92f84901ec9c21832b56aa23b1b7ba14e5667
Jeff King <jeffking@gmail.com>
2020-06-06 13:43:57 -0700

    Add api comment.

--- src/extension/index.d.ts
+++ src/extension/index.d.ts
@@ -4,6 +4,10 @@
 import { Uri } from 'vscode'
 
 export interface Api {
+	/**
+	 * Note: If a log has been modified after open was opened, a close and re-open will be required to "refresh" that log.
+	 * @param logs An array of Uris to open.
+	 */
 	openLogs(logs: Uri[]): Promise<void>
 	closeLogs(logs: Uri[]): Promise<void>
 	closeAllLogs(): Promise<void>
