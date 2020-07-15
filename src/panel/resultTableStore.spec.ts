a83b32475cfd5844b46f5ae450f4f6b8d3809a3a
Mukul Raina <muraina@microsoft.com>
2020-07-15 17:20:52 -0400

    WIP - tests for result table store

---
+++ src/panel/resultTableStore.spec.ts
@@ -0,0 +1,34 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { Row } from './tableStore';
+import { observable } from 'mobx';
+import { _Region, filtersRow, filtersColumn } from '../shared';
+import { ResultTableStore } from './resultTableStore';
+import { log } from '../test/mockResultTableStore';
+import assert from 'assert';
+
+describe('ResultTableStore', () => {
+    it('should verify the default columns', () => {
+        const result1 = log.runs![0].results![0];
+        const result2 = log.runs![0].results![1];
+        const resultsSource = {
+            results: [result1, result2]
+        }
+        const selection = observable.box<Row | undefined>(undefined)
+        const filtersSource = {
+            keywords: 'keyword1',
+            filtersRow: filtersRow,
+            filtersColumn: filtersColumn
+        }
+        const resultTableStore = new ResultTableStore('File', result => result._relativeUri, resultsSource, filtersSource, selection)
+
+        assert.strictEqual(resultTableStore.columns.length, 6);
+        assert.strictEqual(resultTableStore.columns[0].name, 'Line')
+        assert.strictEqual(resultTableStore.columns[1].name, 'File')
+        assert.strictEqual(resultTableStore.columns[2].name, 'Message')
+        assert.strictEqual(resultTableStore.columns[3].name, 'Baseline')
+        assert.strictEqual(resultTableStore.columns[4].name, 'Suppression')
+        assert.strictEqual(resultTableStore.columns[5].name, 'Rule')
+    });
+});
---
+++ src/test/mockResultTableStore.ts
@@ -0,0 +1,107 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { Log } from 'sarif';
+
+export const log = {
+    version: '2.1.0',
+    runs: [{
+        tool: {
+            driver: { name: 'Driver' }
+        },
+        results: [{
+            message: {
+                text: 'Message 1'
+            },
+            locations: [{
+                physicalLocation: {
+                    artifactLocation: {
+                        uri: '/folder/file_1.txt',
+                    }
+                }
+            }]
+        },{
+            message: {
+                text: 'Message 2'
+            },
+            locations: [{
+                physicalLocation: {
+                    artifactLocation: {
+                        uri: '/folder/file_2.txt',
+                    }
+                }
+            }]
+        }, {
+            message: {
+                text: 'Message 2'
+            },
+            locations: [{
+                physicalLocation: {
+                    artifactLocation: {
+                        uri: '/folder/file_2.txt',
+                    }
+                }
+            }]
+        },{
+            message: {
+                text: 'Message 3'
+            },
+            locations: [{
+                physicalLocation: {
+                    artifactLocation: {
+                        uri: '/folder/file_3.txt',
+                    }
+                }
+            }]
+        }, {
+            message: {
+                text: 'Message 3'
+            },
+            locations: [{
+                physicalLocation: {
+                    artifactLocation: {
+                        uri: '/folder/file_3.txt',
+                    }
+                }
+            }]
+        }, {
+            message: {
+                text: 'Message 3'
+            },
+            locations: [{
+                physicalLocation: {
+                    artifactLocation: {
+                        uri: '/folder/file_3.txt',
+                    }
+                }
+            }]
+        }]
+    }]
+} as Log;
+
+export const filtersRow = {
+    Level: {
+        'Error': true,
+        'Warning': true,
+        'Note': true,
+        'None': true,
+    },
+    Baseline: {
+        'New': true,
+        'Unchanged': true,
+        'Updated': true,
+        'Absent': false,
+    },
+    Suppression: {
+        'Not Suppressed': true,
+        'Suppressed': false,
+    }
+}
+
+export const filtersColumn = {
+    Columns: {
+        'Baseline': false,
+        'Suppression': false,
+        'Rule': false,
+    }
+}
