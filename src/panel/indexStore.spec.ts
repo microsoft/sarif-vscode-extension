347d81755bc2274e2ef342e9bfde835402739fdc
Mukul Raina <muraina@microsoft.com>
2020-07-19 01:59:51 -0400

    Add tests for grouping of results and filtering

--- src/panel/indexStore.spec.ts
+++ src/panel/indexStore.spec.ts
@@ -3,18 +3,37 @@
 
 import { filtersColumn, filtersRow } from '../shared';
 import { IndexStore } from './indexStore';
-import { log, log2 } from '../test/mockLog';
+import { log, log2, log3 } from '../test/mockLog';
 import assert from 'assert';
 
 describe('IndexStore', () => {
-    it('updates the results as new logs are added', () => {
+    it('Keeps track of Log[] data and extract and flattens Result[] from it ', () => {
         const indexStore = new IndexStore({ filtersRow, filtersColumn });
         indexStore.logs.push(log);
         assert.strictEqual(indexStore.results.length, 1);
         assert.deepStrictEqual(indexStore.results.map((result) => result.message.text), ['Message 1']);
-
         indexStore.logs.push(log2);
         assert.strictEqual(indexStore.results.length, 2);
         assert.deepStrictEqual(indexStore.results.map((result) => result.message.text), ['Message 1', 'Message 2']);
+    });
+    it('Consists of 2 ResultTableStores grouped by artifact location and rule', () => {
+        const indexStore = new IndexStore({ filtersRow, filtersColumn });
+        indexStore.logs.push(log);
+        assert.strictEqual(indexStore.resultTableStoreByLocation.rows.length, 2);
+        assert.strictEqual(indexStore.resultTableStoreByRule.rows.length, 2);
+        indexStore.logs.push(log2);
+        assert.strictEqual(indexStore.resultTableStoreByLocation.rows.length, 4);
+        assert.strictEqual(indexStore.resultTableStoreByRule.rows.length, 3);
+    });
+    it('can filter results based on keywords', () => {
+        const indexStore = new IndexStore({ filtersRow, filtersColumn });
+        indexStore.logs.push(log, log2, log3);
+        assert.strictEqual(indexStore.resultTableStoreByLocation.groupsFilteredSorted.length, 3);
+        assert.deepStrictEqual(indexStore.resultTableStoreByLocation.groupsFilteredSorted.map((rowGroup) => rowGroup.title), ['/file.txt', '/file_2.txt', '/file_3.txt']);
+        indexStore.keywords = 'file_3';
+        assert.strictEqual(indexStore.resultTableStoreByLocation.groupsFilteredSorted.length, 1);
+        assert.deepStrictEqual(indexStore.resultTableStoreByLocation.groupsFilteredSorted.map((rowGroup) => rowGroup.title), ['/file_3.txt']);
+        indexStore.clearFilters();
+        assert.strictEqual(indexStore.resultTableStoreByLocation.groupsFilteredSorted.length, 3);
     });
 });
--- src/test/mockLog.ts
+++ src/test/mockLog.ts
@@ -31,3 +31,4 @@ const generateMockLog = (version: string, driver: string, message: string, uri: 
 
 export const log = generateMockLog('2.1.0', 'Driver', 'Message 1', '/folder/file.txt', 1);
 export const log2 = generateMockLog('2.1.0', 'Driver', 'Message 2', '/folder/file_2.txt', 18);
+export const log3 = generateMockLog('2.1.0', 'Driver', 'Message 3', '/folder/file_3.txt', 28);
