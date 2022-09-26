e3e0f352e3f023d42f175b2eb809f05b69b84992
Jeff King <jeffking@gmail.com>
2022-09-26 16:54:27 -0700

    Temp hardcode pre-release to 11.

--- .github/workflows/bumpPrerelease/action.js
+++ .github/workflows/bumpPrerelease/action.js
@@ -10,26 +10,7 @@ const { parse } = require('semver');
 const package = JSON.parse(readFileSync('./package.json', 'utf8'));
 const packageVer = parse(package.version);
 
-let prerelease = 0; // Main was changed, or no prev version, restart prerelease from 0.
-try {
-	core.startGroup('Fetching tags');
-	execFileSync('git', ['fetch', '--tags']);
-	core.endGroup();
-
-	core.startGroup('Looking for tags from commit history');
-	core.info(execFileSync('git', ['log', '--oneline'], { encoding: 'utf8' }));
-	// `abbrev=0` finds the closest tagname without any suffix.
-	// HEAD~1 assuming the latest commit hasn't been tagged by this Action yet.
-	const tag = execFileSync('git', ['describe', '--tags', '--abbrev=0', 'HEAD~1'], { encoding: 'utf8' }).trim();
-	core.info('Tag for HEAD~1', tag);
-	const lastReleaseVer = parse(tag);
-	if (packageVer.compareMain(lastReleaseVer) === 0) {
-		prerelease = lastReleaseVer.prerelease[0] + 1; // Main is equal, auto-increment the prerelease.
-	}
-	core.endGroup();
-} catch (error) {
-}
-
+let prerelease = 11; // Main was changed, or no prev version, restart prerelease from 0.
 packageVer.prerelease = [ prerelease ];
 package.version = packageVer.format();
 core.info(`Computed package version: ${package.version}`);
