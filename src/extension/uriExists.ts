e09eed0fc29709ddafb0aef64ab903270c454d5a
Winston Liu <winstonliu@microsoft.com>
2023-06-28 11:15:20 -0700

    Honor extension-provided uriBase (#508)
    * Part 1: move uriBase logic into uriRebaser
    
    * Step 2: Add translations
    
    * Disable more schemes
    
    * Also add translations for workspace prefixes
    
    * Add support for uriBase
    
    * Remove overrideUriBase
    
    * Correct decorations URI checking
    
    * Fix URI rebasing
    
    * Relax strategy restrictions
    
    * Clean up an any
    
    * Remove now-unused Array.commonLength
    
    * Allow file system strategy to work with filepaths
    
    * Update localUri to be a real Uri
    
    * Check uriBase before other strategies
    
    * uriBase must be an absolute URI
    
    * Remove bad comment
    
    * Remove unused url-join
    
    * Fix compile
    
    * Can't use Uris as map keys

--- src/extension/uriExists.ts
+++ src/extension/uriExists.ts
@@ -5,9 +5,9 @@ import { Uri, workspace } from 'vscode';
 
 // Hacky: We are using `fs.stat` to test the existence of documents as VS Code does not provide a dedicated existence API.
 // The similar Node `fs` API does not resolve custom URI schemes in the same way that VS Code does otherwise we would use that.
-export default async function uriExists(absoluteUri: string) {
+export default async function uriExists(absoluteUri: Uri) {
     try {
-        await workspace.fs.stat(Uri.parse(absoluteUri, true));
+        await workspace.fs.stat(absoluteUri);
     } catch (error) {
         return false;
     }
