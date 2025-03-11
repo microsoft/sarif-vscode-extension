7600d7a73791d089ef2e45b2efb815e57a8e2626
Carson Radtke <nosrac925@gmail.com>
2025-03-11 18:20:09 -0500

    feat: add justification to SARIF info table (#584)
    * feat: add justification to SARIF info table
    
    * small cleanup
    
    * Update src/panel/details.tsx
    
    Co-authored-by: Andrew Eisenberg <aeisenberg@github.com>
    
    * Update src/shared/index.ts
    
    Co-authored-by: Andrew Eisenberg <aeisenberg@github.com>
    
    ---------
    
    Co-authored-by: Andrew Eisenberg <aeisenberg@github.com>

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -45,6 +45,7 @@ declare module 'sarif' {
         _message: string; // '—' if empty.
         _markdown?: string;
         _suppression?: 'not suppressed' | 'suppressed';
+        _justification?: string;
     }
 }
 
@@ -132,6 +133,17 @@ export function augmentLog(log: Log, rules?: Map<string, ReportingDescriptor>, w
             result._suppression = !result.suppressions || result.suppressions.every(sup => sup.status === 'rejected')
                 ? 'not suppressed'
                 : 'suppressed';
+            result._justification = undefined;
+            if (result.suppressions)
+            {
+                const justifications = result.suppressions
+                    .map(sup => sup?.justification)
+                    .filter(justification => !!justification);
+                if (justifications.length > 0)
+                {
+                    result._justification = justifications[0];
+                }
+            }
         });
     });
     log._distinct = mapDistinct(fileAndUris);
